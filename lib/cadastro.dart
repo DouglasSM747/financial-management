128aa1417766483a44c49e6df8b61b424f967704
larissapessoa <lsp@icomp.ufam.edu.br>
2020-04-11 01:18:02 -0400

    register page

---
+++ lib/cadastro.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Cadastro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tela Cadastro',
+      home: CadastroPage(),
+    );
+  }
+}
+
+class CadastroPage extends StatefulWidget {
+  @override
+  _CadastroPageState createState() => _CadastroPageState();
+}
+
+class _CadastroPageState extends State<CadastroPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                child: Image.asset(
+                  'assets/images/1.png',
+                ),
+                width: MediaQuery.of(context).size.width - 180,
+                height: 80,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      child: this.formText("Name", Icons.account_box),
+                      width: MediaQuery.of(context).size.width - 15,
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    SizedBox(
+                      child: this.formText("Email", Icons.email),
+                      width: MediaQuery.of(context).size.width - 15,
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    SizedBox(
+                      child: this.formText("Password", Icons.lock),
+                      width: MediaQuery.of(context).size.width - 15,
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    SizedBox(
+                      child: this.formText("Confirm password", Icons.lock),
+                      width: MediaQuery.of(context).size.width - 15,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                        width: MediaQuery.of(context).size.width - 80,
+                        child: RaisedButton(
+                          onPressed: () {
+                            // Validate will return true if the form is valid, or false if
+                            // the form is invalid.
+                            if (_formKey.currentState.validate()) {
+                              // Process data.
+                            }
+                          },
+                          /*  */
+                          child: Text('Sign Up'),
+                        ))
+                    /*  */
+                  ],
+                ),
+              )
+            ],
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.only(top: 100),
+        ),
+      ),
+    );
+  }
+
+  TextFormField formText(String texto, IconData icon, {bool password = false}) {
+    return TextFormField(
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          borderSide: BorderSide(color: Colors.white, width: 1.5),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          borderSide: BorderSide(color: Colors.white, width: 1.5),
+        ),
+        hintText: texto,
+        prefixIcon: Icon(
+          icon,
+          color: Colors.white,
+        ),
+        hintStyle: TextStyle(color: Colors.green),
+      ),
+      style: TextStyle(color: Colors.green),
+      obscureText: password,
+      textAlign: TextAlign.start,
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some valeu';
+        }
+        return null;
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:financial/cadastro.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Cadastro());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
