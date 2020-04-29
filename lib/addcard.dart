faa23bae1ee7e4f3a0e1de0f3a603b0291ba7a9f
Douglas <dsm.lic17@uea.edu.br>
2020-04-29 01:49:32 -0300

    Fix CheckBox

--- lib/addcard.dart
+++ lib/addcard.dart
@@ -18,7 +18,6 @@ class AddCardPage extends StatefulWidget {
 }
 
 class _AddCardPageState extends State<AddCardPage> {
-
   bool creditCard = false;
   bool debitCard = false;
 
@@ -32,20 +31,20 @@ class _AddCardPageState extends State<AddCardPage> {
         },
         child: SingleChildScrollView(
           child: Container(
-            padding: EdgeInsets.only(top: 60),
             color: Colors.black,
+            padding: EdgeInsets.only(top: 60),
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 SizedBox(
-                  child : this.textField("Card name", Icons.card_giftcard),
+                  child: this.textField("Card name", Icons.card_giftcard),
                   width: MediaQuery.of(context).size.width - 15,
                 ),
-                this.addCheckbox(creditCard, this.creditCard, "Credit Card"),
-                this.addCheckbox(debitCard, this.debitCard, "Debit Card"),
-              /* Column(
+                this.addCheckbox(this.creditCard, "Credit Card"),
+                this.addCheckbox(this.debitCard, "Debit Card"),
+                /* Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text("Credit Card", style: TextStyle(fontSize: 21, color: Colors.white)),
@@ -59,14 +58,14 @@ class _AddCardPageState extends State<AddCardPage> {
                   this.checkbox(debitCard, this.debitCard)
                 ],
               ), */
-              SizedBox(
-                child: this.infoCards(this.creditCard, "Limit"),
-                width: MediaQuery.of(context).size.width - 15,
-              ),
-              SizedBox(
-                child: this.infoCards(this.debitCard, "Balance"),
-                width: MediaQuery.of(context).size.width - 15,
-              )
+                SizedBox(
+                  child: this.infoCards(this.creditCard, "Limit"),
+                  width: MediaQuery.of(context).size.width - 15,
+                ),
+                SizedBox(
+                  child: this.infoCards(this.debitCard, "Balance"),
+                  width: MediaQuery.of(context).size.width - 15,
+                )
               ],
             ),
           ),
@@ -89,51 +88,74 @@ class _AddCardPageState extends State<AddCardPage> {
     );
   } */
 
-  Widget addCheckbox(option, bool val, String text){
-    return Column( 
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:[ Transform.scale( 
-              scale: 1.5,
-              child: Checkbox(
-                value: option,
-                onChanged: (bool value){setState(() {
-                  val = value;
-                });},
-                activeColor: Colors.green,
-                checkColor: Colors.white,
-                tristate: false,
-              ),
-          ), 
- 
-        Text(text, 
-          style: TextStyle(fontSize: 21), 
-          textAlign: TextAlign.center,)
- 
-      ]);
-  }
-
-
-  TextField textField(String text, IconData icon){
-    return TextField(
-        decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          borderSide: BorderSide(color: Colors.white, width: 1.5),
+  Widget addCheckbox(bool val, String text) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(height: 10),
+        Container(
+          height: 30,
+          width: 30,
+          decoration: const BoxDecoration(
+            border: Border(
+              top: BorderSide(width: 3.0, color: Colors.white),
+              left: BorderSide(width: 3.0, color: Colors.white),
+              right: BorderSide(width: 3.0, color: Colors.white),
+              bottom: BorderSide(width: 3.0, color: Colors.white),
+            ),
+          ),
+          child: Transform.scale(
+            scale: 1.7,
+            child: Checkbox(
+              value: val,
+              onChanged: (bool value) {
+                if (text == "Credit Card") {
+                  setState(() {
+                    creditCard = value;
+                  });
+                }
+                if (text == "Debit Card") {
+                  setState(() {
+                    debitCard = value;
+                  });
+                }
+              },
+              focusColor: Colors.white,
+              hoverColor: Colors.white,
+              activeColor: Colors.green,
+              checkColor: Colors.white,
+            ),
+          ),
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          borderSide: BorderSide(color: Colors.white, width: 1.5),
-        ),
-        hintText: text,
-        hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
-        prefixIcon: Icon(icon, color: Colors.white))
+        Text(
+          text,
+          style: TextStyle(fontSize: 21, color: Colors.white),
+          textAlign: TextAlign.center,
+        )
+      ],
     );
   }
 
-  Widget infoCards(bool type, String text){
-    if(type){
+  TextField textField(String text, IconData icon) {
+    return TextField(
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              borderSide: BorderSide(color: Colors.white, width: 1.5),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              borderSide: BorderSide(color: Colors.white, width: 1.5),
+            ),
+            hintText: text,
+            hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+            prefixIcon: Icon(icon, color: Colors.white)));
+  }
+
+  Widget infoCards(bool type, String text) {
+    if (type) {
       return Container(
-        child : this.textField(text, Icons.monetization_on),
+        child: this.textField(text, Icons.monetization_on),
         width: MediaQuery.of(context).size.width - 15,
       );
     }
